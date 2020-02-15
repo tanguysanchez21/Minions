920aba2c651cf8ce0c8a4b95d79e6b18fba73377
tanguy21 <tanguysanchez21@gmail.com>
2020-02-16 00:27:04 +0100

    first commit

---
+++ Assets/Scripts/Astar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum TileType { WATER, GRASS, DIRT }
+
+public class Astar : MonoBehaviour
+{
+    private TileType tileType;
+
+    [SerializeField]
+    private Tilemap tilemap;
+
+    [SerializeField]
+    private Tile[] tiles;
+
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, layerMask);
+
+            if(hit.collider != null)
+            {
+                Vector3 mouseWorldPos = camera.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int clickPos = tilemap.WorldToCell(mouseWorldPos);
+
+                //ChangeTile(clickPos);
+            }
+        }
+    }
+
+    private void ChangeTile(Vector3Int clickPos)
+    {
+        tilemap.SetTile(clickPos, tiles[(int)tileType]);
+    }
+}
